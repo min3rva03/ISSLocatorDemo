b9333bac7393b6ff6b04e5f8062e8f03df2618ae
Minerva <msnolasco@icloud.com>
2022-05-10 20:24:29 -0500

    Initial Commit

---
+++ IssLocator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IssLocator
+//
+//  Created by Minerva Nolasco Espino on 10/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
