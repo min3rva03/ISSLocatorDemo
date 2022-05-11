bd613e3986e73cfca2bd33353b215759d066c738
Minerva <msnolasco@icloud.com>
2022-05-10 23:25:06 -0500

    Se remueve APIKey de archivo AppDelegate

--- IssLocator/AppDelegate.swift
+++ IssLocator/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyBuOE9HCyH2GKGKLYFf-CFgxqlN83ktH_E")
+        GMSServices.provideAPIKey("")
         return true
     }
 }
